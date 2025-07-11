37dea92e0b0895d76a3ca3444f10096089c3140d
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-07-11 15:14:16 +0300

    updated landing page

--- devlink-secure/src/components/Fragments.jsx
+++ devlink-secure/src/components/Fragments.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
 
 export const Fragments = () => {
+  const dotCount = 20
+
+  // Random dot positions
+  const dots = Array.from({ length: dotCount }, (_, i) => ({
+    id: i,
+    top: Math.random() * 100,
+    left: Math.random() * 100
+  }))
+
   return (
-    <div className='text-white bg-white/50 w-[370px] md:w-[1800px] h-full'>Fragments</div>
+    <div className='relative text-white w-[370px] rounded md:w-[1800px] h-[100vh] overflow-hidden bg-black'>
+      <span className='absolute top-4 left-4 z-10'></span>
+
+      {/* Glowing Lines */}
+      <svg className='absolute inset-0 w-full h-full z-0 pointer-events-none'>
+        {dots.map((dotA, i) =>
+          dots.map((dotB, j) =>
+            i < j ? (
+              <line
+                key={`${i}-${j}`}
+                x1={`${dotA.left}%`}
+                y1={`${dotA.top}%`}
+                x2={`${dotB.left}%`}
+                y2={`${dotB.top}%`}
+                stroke='cyan'
+                strokeWidth='0.5'
+                strokeOpacity='0.3'
+              />
+            ) : null
+          )
+        )}
+      </svg>
+
+      {/* Glowing Dots */}
+      {dots.map(dot => (
+        <div
+          key={dot.id}
+          className='absolute w-2 h-2 bg-teal-600 rounded-full animate-pulse z-10'
+          style={{
+            top: `${dot.top}%`,
+            left: `${dot.left}%`,
+            boxShadow: '0 0 12px rgba(0,255,255,0.8)'
+          }}
+        />
+      ))}
+    </div>
   )
 }
-
--- devlink-secure/src/pages/Home.jsx
+++ devlink-secure/src/pages/Home.jsx
@@ -5,13 +5,28 @@ import { Fragments } from '../components/Fragments'
 function Home() {
     return (
         <>
-                {/* <Header/> */}
-                <div className='h-screen flex bg-black'>
-                    <Header/>
-                    <div className='overflow-x-hidden mt-12 md:mt-[6%] ml-[2%]'>
-                        <Fragments/>
-                    </div>
+            {/* <Header/> */}
+            <div className='h-screen relative flex bg-black p-3'>
+                <Header />
+
+                {/* Fragments should appear underneath */}
+                <div className='overflow-hidden mt-12 md:mt-[6%] ml-6 z-10 relative'>
+                    <Fragments />
                 </div>
+
+                {/* Section placed absolutely on top */}
+                <section className="absolute top-0 left-0 w-full h-full z-20 bg-[rgba(43,42,42,0.7)] flex justify-center">
+                    
+                    
+                </section>
+
+            </div>
+
+
+            {/* about us section */}
+            <section className='h-screen'>
+
+            </section>
         </>
     )
 }
